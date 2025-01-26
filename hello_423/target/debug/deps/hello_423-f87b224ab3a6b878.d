8f309792615a48f39cecadebdcc5de3349bf4005
kawbu <kawbu0823@gmail.com>
2025-01-26 23:34:03 +0000

    complete rust tutorial

---
+++ hello_423/target/debug/deps/hello_423-f87b224ab3a6b878.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello_423/target/debug/deps/hello_423-f87b224ab3a6b878: src/main.rs
+
+/workspaces/rust-tutorial/hello_423/target/debug/deps/hello_423-f87b224ab3a6b878.d: src/main.rs
+
+src/main.rs:
---
+++ hello_423/target/debug/hello_423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial/hello_423/target/debug/hello_423: /workspaces/rust-tutorial/hello_423/src/main.rs
